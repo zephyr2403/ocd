dec44c5066739a793a661278223cab9fa23f3038
EverWinter23 <eternal.blizzard23@gmail.com>
2018-01-27 16:22:01 +0530

    setup.py: Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='ocd',
+           version='1.0.0',
+           description='Open current directory',
+           author='rishabh',
+           author_email='eternal.blizzard23@gmail.com',
+           scripts = [
+                'bin/ocd'
+           ]
+           )
